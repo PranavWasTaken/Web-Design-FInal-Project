c45a2842c5447e3492b2b415fa8ef33491e6a01b
Pranav Kulkarni <kulkarni.pranav@northeastern.edu>
2023-04-17 16:17:03 -0400

    Update user.model.js

--- movies-rest/app/models/user.model.js
+++ movies-rest/app/models/user.model.js
@@ -17,3 +17,31 @@ const UserSchema = mongoose.Schema({
   followers: [{userId: {type: 'ObjectId', required: true}, username: {type: String, required: true}}],
   following: [{userId: {type: 'ObjectId', required: true}, username: {type: String, required: true}}]
 });
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
