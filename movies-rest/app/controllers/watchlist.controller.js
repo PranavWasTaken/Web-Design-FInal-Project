ce40a043b60466a8fa0ba9401b3beca240916230
Pranav Kulkarni <kulkarni.pranav@northeastern.edu>
2023-04-18 14:57:40 -0400

    Update watchlist.controller.js

--- movies-rest/app/controllers/watchlist.controller.js
+++ movies-rest/app/controllers/watchlist.controller.js
@@ -16,3 +16,22 @@ exports.addToWatchlist = (req, res) => {
     .catch(err=>res.status(500).send({message: err.message}));
 
 }
+
+exports.removeFromWatchlist = (req, res) => {
+    let userId = req.params.userId;
+    let movieId = req.params.movieId;
+  
+    User.findById(userId)
+      .then(user=>{
+        let i = 0;
+        for(i = 0;i<user.watchlist.length;i++){
+          if(user.watchlist[i].movieId==movieId){
+            break;
+          }
+        }
+        user.watchlist.pull({_id: user.watchlist[i]._id, movieId: movieId});
+        return user.save();
+      })
+      .then(()=>res.status(200).send({message: "Remove from watchlist successfully"}))
+      .catch(err=>res.status(500).send({message: err.message}));
+  }
