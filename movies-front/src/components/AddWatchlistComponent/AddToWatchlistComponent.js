7ecddf6ffb80fd04b051a4de0141eda05a8b123c
Pranav Kulkarni <kulkarni.pranav@northeastern.edu>
2023-04-18 11:59:01 -0400

    Update AddToWatchlistComponent.js

--- movies-front/src/components/AddWatchlistComponent/AddToWatchlistComponent.js
+++ movies-front/src/components/AddWatchlistComponent/AddToWatchlistComponent.js
@@ -14,3 +14,33 @@ const defaultOptions = {
         preserveAspectRatio: 'xMidYMid slice'
       }
   };
+  export default class AddToWatchlistComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userProfile: '',
+            watchlistIds: []
+        }
+
+        this.userService = new UserService();
+
+        this.addToWatchlist = this.addToWatchlist.bind(this);
+        this.removeFromWatchlist = this.removeFromWatchlist.bind(this);
+    }
+
+    componentDidMount() {
+        this.userService.findUserById(this.props.userId).then(response => {
+
+            let idArray = [];
+            for (let movie in response.watchlist) {
+                idArray.push(response.watchlist[movie].movieId);
+            }
+            this.setState({
+                userProfile: response,
+                watchlistIds: idArray
+            })
+
+            }
+        )
+    }
+  }
