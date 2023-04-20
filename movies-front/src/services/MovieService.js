220a75f429774dfcb5b6f4fcaccb5adc255a9f19
GargavaSiddharthNEU <114346515+GargavaSiddharthNEU@users.noreply.github.com>
2023-04-19 20:23:57 -0400

    movie service update

---
+++ movies-front/src/services/MovieService.js
@@ -0,0 +1,42 @@
+import * as constants from '../services/Constants'
+
+let _singleton = Symbol();
+
+class MovieServiceClient {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
+
+    static get instance() {
+        if (!this[_singleton])
+            this[_singleton] = new MovieServiceClient(_singleton);
+        return this[_singleton]
+    }
+
+    getPopularMovies() {
+        return fetch(constants.POPULAR_MOVIES_URL, {
+            method: 'GET'
+        }).then(function (response) {
+            if(response.headers.get("content-type")!=null)
+                return response.json();
+            else return null;
+        });
+    }
+
+    getNowPlayingMovies() {
+        return fetch(constants.NOW_PLAYING_MOVIES_URL, {
+            method: 'GET'
+        }).then(function (response) {
+            if(response.headers.get("content-type")!=null)
+                return response.json();
+            else return null;
+        });
+    }
+
+
+
+
+}
+
+    export default MovieServiceClient;
